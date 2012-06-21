364db8d80b549569dc337b4081dbd60720eddfb1
Sam Liu <sam@ambushnetworks.com>
2012-06-21 14:34:52 -0700

    basic crawler prints out all the info

--- spade/scraper/scraper/spiders/general_spider.py
+++ spade/scraper/scraper/spiders/general_spider.py
@@ -1,7 +1,11 @@
 # General spider for retrieving site information
 
 from scrapy.spider import BaseSpider
+import zlib
+import json
+import urlparse
 
+USER_AGENT='android'
 
 class GeneralSpider(BaseSpider):
     name = "dmoz"
@@ -11,6 +15,22 @@ class GeneralSpider(BaseSpider):
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
 
+    # Get's a content type from the headers
+    def get_content_type(self, headers):
+        if headers:
+            for h in headers:
+                if h.lower().strip() == 'content-type':
+                    return headers[h]
+
+    # Gzip a json object for storage
+    def gzipped_json(obj):
+        s = json.dumps(obj, ensure_ascii=False)
+        return zlib.compress(s)
+
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+        print response.url
+        print response.body
+        print response.headers
+        print self.get_content_type(response.headers)
+        print response.meta.get('referrer')
+        print USER_AGENT
