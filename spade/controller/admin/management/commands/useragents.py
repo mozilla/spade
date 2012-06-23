9db0fa804d172c186c42352e883117f196909147
Sam Liu <sam@ambushnetworks.com>
2012-06-22 17:41:51 -0700

    added manage command that allows adding and removing useragents

---
+++ spade/controller/admin/management/commands/useragents.py
@@ -0,0 +1,55 @@
+import os
+
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
+from spade.model.models import UserAgent
+
+class Command(BaseCommand):
+    help = "Manage user agents to use in each crawl."
+
+    option_list = BaseCommand.option_list + (
+        make_option('--new',
+                    action='store',
+                    dest='new',
+                    default=False,
+                    help='Insert new useragent string to include'),
+        make_option('--list',
+                    action='store_true',
+                    dest='list',
+                    default=False,
+                    help='List currently indexed useragent strings'),
+        make_option('--remove',
+                    action='store',
+                    dest='remove',
+                    default=False,
+                    help='Remove useragent string from database')
+        )
+
+    def handle(self, *args, **options):
+
+        new = options.get('new')
+        remove = options.get('remove')
+
+        if options.get('list'):
+            self.stdout.write("Listing all saved user agent strings:\n")
+            self.stdout.write("=====================================\n")
+
+            for agent in UserAgent.objects.all():
+                self.stdout.write(agent.ua_string+'\n')
+
+        elif new:
+            new_ua = UserAgent()
+            new_ua.ua_string = str(new)
+            new_ua.save()
+            self.stdout.write('Successfully inserted "%s"\n' % str(new))
+
+        elif remove:
+            try:
+                ua_to_remove = UserAgent.objects.get(ua_string=remove)
+            except UserAgent.DoesNotExist:
+                raise CommandError("No such UA string exists.")
+
+            ua_to_remove.delete()
+            self.stdout.write('Successfully removed "%s"\n' % str(remove))
+        else:
+            raise CommandError("You must give a valid parameter.")
