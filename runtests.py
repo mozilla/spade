8472aa79660583e380a0bcf87bc2bfcbfe47a7c7
Carl Meyer <carl@oddbird.net>
2012-06-26 16:04:58 -0600

    Add runtests.py and sample test.

---
+++ runtests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Run py.test after setting up the vendor library and DJANGO_SETTINGS_MODULE.
+
+"""
+import os
+import sys
+
+from _pytest.core import main as pytest_main
+
+from spade.vendor import add_vendor_lib
+
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spade.settings.default")
+    add_vendor_lib()
+    args = sys.argv[1:]
+    if not args:
+        args = ["spade/tests/"]
+    raise SystemExit(pytest_main(args))
+
+
+if __name__ == "__main__":
+    main()
