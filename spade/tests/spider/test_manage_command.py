fd87c2804c22f0f350ac7ab7d213e03a7f79a625
Sam Liu <sam@ambushnetworks.com>
2012-07-02 15:26:29 -0700

    minor name change

--- spade/tests/spider/test_manage_command.py
+++ spade/tests/spider/test_manage_command.py
@@ -5,7 +5,7 @@ Test manage.py hook to scrapy
 from django.core.management import call_command
 
 def test_manage_hook():
-    call_command('scraper', 'sitelists/all.txt')
+    call_command('scrape', 'sitelists/all.txt')
 
     # STUB until I know how to test this
     assert True
