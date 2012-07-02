42a738ae21fbec220b160e05369f55719202bf09
Sam Liu <sam@ambushnetworks.com>
2012-07-02 15:00:14 -0700

    useful help message, using args instead of self._argv

--- spade/controller/management/commands/scrape.py
+++ spade/controller/management/commands/scrape.py
@@ -4,21 +4,26 @@ Just a thin wrapper around scrapy, so we can run it as a management command.
 """
 from __future__ import absolute_import
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
-    def run_from_argv(self, argv):
-        self._argv = argv
-        self.execute()
+    args = "<newline delimited textfile of URLs to scrape>"
+    help = (
+        "  Scrapes and parses html/css properties down to 1-level\n"
+        "  on each site from the specified text file\n")
 
     def handle(self, *args, **options):
-        from scrapy.cmdline import execute
+        if (not len(args) == 1) or (args[0] == "help"):
+            self.stdout.write("Usage: {0}\n".format(self.args))
+            self.stdout.write(self.help)
+        else:
+            # Take a filename from command line to crawl
+            default = [u""]
+            default.append(u"crawl")
+            default.append(u"all")
+            default.append(u"-s")
+            default.append(u"URLS="+unicode(args[0]))
 
-        # Take a filename from command line to crawl
-        default = [u""]
-        default.append(u"crawl")
-        default.append(u"all")
-        default.append(u"-s")
-        default.append(u"URLS="+unicode(self._argv[2]))
-        execute(default)
+            from scrapy.cmdline import execute
+            execute(default)
