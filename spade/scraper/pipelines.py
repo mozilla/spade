fd2e2c0820e9f31d82641df50aeb447ebe2eab99
Sam Liu <sam@ambushnetworks.com>
2012-07-02 18:21:18 -0700

    removed old comments

--- spade/scraper/pipelines.py
+++ spade/scraper/pipelines.py
@@ -4,5 +4,13 @@
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
 class ScraperPipeline(object):
+    def open_spider(spider):
+        pass
+        # open batch
+
+    def close_spider(spider):
+        pass
+        # close batch
+
     def process_item(self, item, spider):
         return item
