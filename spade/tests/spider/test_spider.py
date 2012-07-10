d6e09f1538bf81716c4891096738a97f6ce5209a
Sam Liu <sam@ambushnetworks.com>
2012-07-10 15:06:03 -0700

    writing test for item pipeline

--- spade/tests/spider/test_spider.py
+++ spade/tests/spider/test_spider.py
@@ -1,4 +1,5 @@
 from scrapy.conf import settings
+from scrapy.http import Response, Request
 from spade.scraper.spiders.general_spider import GeneralSpider
 
 def pytest_funcarg__spider(request):
@@ -23,10 +24,52 @@ def test_read_from_file(spider):
     else:
         assert False
 
-def test_useragents_pipeline(spider):
+def test_useragents_pipeline(spider, httpserver):
     """Ensure multiple items with different user agent strings are emitted"""
-    # This needs to test that items sent to the item pipeline have different user
-    # agents
+    # This needs to test that items sent to the item pipeline have different
+    # user agents
+
+    mock_html = """
+                <html>
+                <head></head>
+                <body>
+                <a href="link1.html">Link 1</a>
+                <a href="link2.html">Link 2</a>
+                </body></html>
+                """
+
+    mock_headers = {'Content-Length': ['102'],
+                    'X-Powered-By': ['PHP/5.3.2-1ubuntu4.17'],
+                    'Vary': ['Accept-Encoding'],
+                    'Server': ['Apache/2.2.14 (Ubuntu)'],
+                    'Connection': ['close'],
+                    'Date': ['Tue, 10 Jul 2012 21:52:28 GMT'],
+                    'Content-Type': ['text/html']}
+
+    httpserver.serve_content(content=mock_content,
+                             code=200,
+                             headers=mock_headers)
+
+    # Create a mock request
+    mock_request = Request(httpserver.url)
+    mock_request.meta = {'referrer':None, 'sitescan':None, 'user_agent':None}
+    mock_request.dont_filter = True
+
+
+    # Create a mock response linked to the request
+    mock_response = Response(httpserver.url)
+    mock_response.meta = {'download_timeout': 20,
+                          'depth': 0,
+                          'download_latency': 0.15383815765380859}
+    mock_response.flags = []
+    mock_response.request = mock_request
+    mock_response.status = 200
+    mock_response.headers = mock_headers
+
+    generator = spider.parse(mock_response)
+    for items_and_requests in generator:
+        print items_and_requests
+
     assert False
 
 def test_savedcontent(spider):
