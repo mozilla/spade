970bb934f3a85aec7679379e4d951253c9f7fcbc
Sam Liu <sam@ambushnetworks.com>
2012-08-07 15:15:31 -0700

    passing mock data to dashboard

--- spade/view/urls.py
+++ spade/view/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', 'spade.view.views.dashboard', name='dashboard'),
     url(r'^batch/(?P<batch_id>\d+)/$', 'spade.view.views.batch_report', name='batch_report'),
     url(r'^site/(?P<site_id>\d+)/$', 'spade.view.views.site_report', name='site_report'),
+    url(r'^site/(?P<site_id>\d+)/(?P<user_agent>\d+)/$', 'spade.view.views.site_report', name='site_report_with_ua'),
 
     url(r'^admin/', include(admin.site.urls)),
 
--- spade/view/views.py
+++ spade/view/views.py
@@ -6,20 +6,47 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
-
+from spade import model
 
 def dashboard(request):
     """ Front page dashboard view """
-    return TemplateResponse(request, "dashboard.html",{})
+
+    batches = model.Batch.objects.order_by('finish_time')
+
+    batch_data = {}
+    for batch in batches:
+        batch_data[batch.finish_time] = {
+            'id': batch.id,
+            'ua_issues_fixed':0,
+            'css_issues_fixed': 0,
+            'ua_issues_regressed':0,
+            'css_issues_regressed':0,
+            'ua_issue_percent':0,
+            'css_issue_percent':0,
+        }
+
+    context = {batch_data}
+
+    # TODO: Determine UA issue %
+    # TODO: Determine CSS issue %
+    # TODO: Determine UA issues fixed since last scan
+    # TODO: Determine CSS issues fixed since last scan
+    # TODO: Determine UA issues regressed
+    # TODO: Determine CSS issues regressed
+
+    return TemplateResponse(request, "dashboard.html", context)
 
 
 def batch_report(request, batch_id):
     """ Batch report view """
     context = { 'batch_id': batch_id }
+
+
     return TemplateResponse(request, "batch_report.html", context)
 
 
-def site_report(request, site_id):
+def site_report(request, site_id, user_agent="combined"):
     """ Site report view """
     context = { 'site_id': site_id }
+    print user_agent
     return TemplateResponse(request, "site_report.html", context)
