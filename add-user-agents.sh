c782ddccc4366803260a4f3e81d193646bae63ff
Aaron Train <aaron.train@gmail.com>
2012-09-20 12:49:33 -0400

    Adjusted Fennec UA

--- add-user-agents.sh
+++ add-user-agents.sh
@@ -9,7 +9,7 @@
 ./manage.py useragents --add "Mozilla/5.0 (Linux; U; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebkit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --name "Chrome 18, Galaxy Nexus"
 
 # Add fennec UA
-./manage.py useragents --add "Mozilla/5.0 (Android; Linux armv7l; rv:15.0) Gecko/20111216 Firefox/15.0 Fennec/15.0" --primary --name "Fennec 15.0"
+./manage.py useragents --add "Mozilla/5.0 (Android; Mobile; rv:15.0) Gecko/15.0 Firefox/15.0" --primary --name "Fennec 15.0"
 
 # Add desktop firefox
 ./manage.py useragents --add "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2" --desktop --name "Firefox Desktop 15.0 Windows"
