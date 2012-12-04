7a84635cb9ef19ea615c4465781348752b8206ea
Clint Talbert <ctalbert@mozilla.com>
2012-12-03 16:59:32 -0800

    Update User agents p=mihneadb r=ctalbert

--- add-user-agents.sh
+++ add-user-agents.sh
@@ -9,10 +9,13 @@
 ./manage.py useragents --add "Mozilla/5.0 (Linux; U; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebkit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" --name "Chrome 18, Galaxy Nexus"
 
 # Add fennec UA
-./manage.py useragents --add "Mozilla/5.0 (Android; Mobile; rv:15.0) Gecko/15.0 Firefox/15.0" --primary --name "Fennec 15.0"
+./manage.py useragents --add "Mozilla/5.0 (Android; Mobile; rv:18.0) Gecko/18.0 Firefox/18.0" --primary --name "Fennec 18.0"
 
 # Add desktop firefox
-./manage.py useragents --add "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2" --desktop --name "Firefox Desktop 15.0 Windows"
+./manage.py useragents --add "Mozilla/5.0 (Windows NT 6.1; rv:18.0) Gecko/20120716 Firefox/18.0" --desktop --name "Firefox Desktop 18.0 Windows"
+
+# Add android stock UA
+./manage.py useragents --add "Mozilla/5.0 (Linux; U; Android 4.0.2; en-US; Galaxy Nexus Build/ICL53F) AppleWebKit/535.7 (KHTML, like Gecko) CrMo/16.0.912.75 Mobile Safari/535.7" --name "Android ICS Stock"
 
 # Add firefox os
-./manage.py useragents --add "Mozilla/5.0 (Mobile; rv:15.0) Gecko/15.0 Firefox/15.0" --name "Firefox OS"
+./manage.py useragents --add "Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0" --name "Firefox OS gecko: 18"
